b1d33bd8168db1bf105c7c67523354180a585cb7
Justin Reasoner <knoxxbox@gmail.com>
2016-01-17 13:57:28 -0500

    Easily remove the box with the remove-box script

---
+++ other/remove-box.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+delete_box()
+{
+        rm -rf ~/Homestead56
+        vagrant global-status --prune
+        VBoxManage controlvm homestead-56 poweroff
+        VBoxManage unregistervm homestead-56 --delete
+        echo "Deleted box! Now remove ~/.homestead-56 to remove previous sites if you like..."
+        exit 0
+}
+
+read -p "Are you sure you want to continue? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+	delete_box()
+else
+  exit 0
+fi
