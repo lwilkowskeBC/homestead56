404445961e1439e43a37528faedb1b25fe8dbaeb
Lawrence Wilkowske <lawrence.wilkowske@bigcommerce.com>
2016-02-02 10:49:50 -0600

    Custom after.sh for Craft project

--- src/stubs/after.sh
+++ src/stubs/after.sh
@@ -3,3 +3,13 @@
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
+
+#Kind of a nuclear option, but will allow you to use the VM DB with Craft
+#See https://craftcms.stackexchange.com/questions/12084/getting-this-sql-error-group-by-incompatible-with-sql-mode-only-full-group-by/12106#comment17942_12106
+echo 'sql_mode=""' >> /etc/mysql/my.cnf
+
+#Server Tweaks
+config='/etc/php5/fpm/php.ini'
+sudo sed -i 's/memory_limit = .*/memory_limit = 512m/' $config
+sudo sed -i 's/; max_input_vars = .*/max_input_vars = 6000/' $config
+
